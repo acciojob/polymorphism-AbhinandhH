b03ac5766a0bc5247cddacc49f8565d3fadb8cc1
Abhinandh Haridas <abhinandhabhi53@gmail.com>
2023-01-12 23:30:27 +0530

    completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
 
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        double a = 1;
+        double b = 2;
+
+        p.product(x, y);
+        p.product(x, y, z);
+        p.product(a , b);
+    }
+}
+class Product{
+    public int product(int x, int y) {
+        return x * y;
+    }
+    public int product(int x, int y, int z) {
+        return x * y * z;
+    }
+    public double product(double x, double y) {
+        return x * y;
+    }
 }
