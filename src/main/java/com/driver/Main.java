0c88ab8adae4e8475e954df6c50915d509b39bea
Abhinandh Haridas <abhinandhabhi53@gmail.com>
2023-01-15 23:25:14 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,21 @@
 package com.driver;
 
 public class Main {
+    public  static class Product{
+        public int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
-
-        int x = 1;
-        int y = 2;
-        int z = 3;
-        double a = 1;
-        double b = 2;
-
-        p.product(x, y);
-        p.product(x, y, z);
-        p.product(a , b);
-    }
-}
-class Product{
-    public int product(int x, int y) {
-        return x * y;
-    }
-    public int product(int x, int y, int z) {
-        return x * y * z;
-    }
-    public double product(double x, double y) {
-        return x * y;
+        p.product(10, 20);
+        p.product(10, 20, 30);
+        p.product(10.0, 20.0);
     }
 }
